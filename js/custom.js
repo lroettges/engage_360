46a8062e98cdc16270e805c7836c983888eebc75
lroettges <lroettges@hotmail.com>
2015-04-16 01:36:44 -0500

    playing around with js

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,41 @@
       });
   });
 // End Navbar Smooth Scroll
+
+// change my homepage
+function whoIsEngage(a, b) {
+    document.getElementById("our_description").innerHTML = "a long winded description";
+    document.getElementById("our_title").innerHTML = "";
+}
+//end my homepage 
+
+//alternating images
+   // Initialization
+$('#vennD li:first').addClass('show');
+$('#vennD li:gt(0)').hide();
+
+function loopImages() {
+    // Retrieve the currently shown <li>, try to find the next <li>
+    var next = $("#vennD li.show").next('li');
+    // If not foudn, then retrieve the first one in the list
+    if (!next.length) next = $("#vennD li:first");
+    // Remove ".show", and hide the current <li>
+    $("#vennD li.show").removeClass('show').fadeOut(500);
+    // Add ".show" and show next <li>
+    next.addClass('show').fadeIn(500);
+}
+
+intervalId = setInterval(loopImages, 1000);
+
+$('.targ').hover(function () {
+    clearInterval(intervalId);
+    var tabIndex = $(this).index();
+    // Hide & remove ".show"
+    $('#vennD li').hide().removeClass('show');
+    // Add ".show" & display targeted <li>
+    $('#vennD li').eq(tabIndex).addClass("show").show();
+}, function () {
+    intervalId = setInterval(loopImages, 1000);
+});
+
+//end alternating images
