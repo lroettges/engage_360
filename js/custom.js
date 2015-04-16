b1b3b91e5e8a8aafbeee75382352e747bc1d67c0
lroettges <lroettges@hotmail.com>
2015-04-16 12:58:15 -0500

    js improved

--- js/custom.js
+++ js/custom.js
@@ -33,7 +33,7 @@
     // animate
     $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 130, function(){
+      }, 1300, function(){
 
         // when done, add hash to url
         // (default click behaviour)
@@ -51,16 +51,16 @@ function whoIsEngage(a, b) {
 
 //alternating images
    // Initialization
-$('#vennD li:first').addClass('show');
-$('#vennD li:gt(0)').hide();
+$('#trashCan li:first').addClass('show');
+$('#trashCan li:gt(0)').hide();
 
 function loopImages() {
     // Retrieve the currently shown <li>, try to find the next <li>
-    var next = $("#vennD li.show").next('li');
+    var next = $("#trashCan li.show").next('li');
     // If not foudn, then retrieve the first one in the list
-    if (!next.length) next = $("#vennD li:first");
+    if (!next.length) next = $("#trashCan li:first");
     // Remove ".show", and hide the current <li>
-    $("#vennD li.show").removeClass('show').fadeOut(500);
+    $("#trashCan li.show").removeClass('show').fadeOut(500);
     // Add ".show" and show next <li>
     next.addClass('show').fadeIn(500);
 }
@@ -71,11 +71,28 @@ $('.targ').hover(function () {
     clearInterval(intervalId);
     var tabIndex = $(this).index();
     // Hide & remove ".show"
-    $('#vennD li').hide().removeClass('show');
+    $('#trashCan li').hide().removeClass('show');
     // Add ".show" & display targeted <li>
-    $('#vennD li').eq(tabIndex).addClass("show").show();
+    $('#trashCan li').eq(tabIndex).addClass("show").show();
 }, function () {
     intervalId = setInterval(loopImages, 1000);
 });
 
 //end alternating images
+
+// change trashCan to text
+function throwTrash(a, b) {
+    document.getElementById("trashCan").innerHTML = " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+}
+//end trashCan to text
+
+// change my homepage
+function boxTops(a, b) {
+    document.getElementById("boxtop").innerHTML = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+//end my homepage 
+
+
+
+
